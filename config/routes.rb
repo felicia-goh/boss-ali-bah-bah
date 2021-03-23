1c001624ff484d1e5fedd6efdfd69b30b3c99e2f
Felicia Goh <minhui.felicia@gmail.com>
2021-03-23 22:59:49 +0800

    fix routes and error pages

--- config/routes.rb
+++ config/routes.rb
@@ -12,18 +12,19 @@ Rails.application.routes.draw do
   get '/personal_details', to: 'users#personal_details', as: 'personal_details'
   patch '/personal_update', to: 'users#personal_update', as: 'personal_update'
 
-  resources :accidents do
-    member do
-      get :new3
-      get :complete
-    end
-  end
+
   get '/accidents/confirm', to: 'accidents#confirm'
   post '/accidents/send', to: 'accidents#send_accident'
   post '/workdays/send', to: 'workdays#send_worklog'
   post '/payslips/send', to: 'payslips#send_payslip'
   post '/users/send', to: 'users#send_ipa'
 
+  resources :accidents do
+    member do
+      get :new3
+      get :complete
+    end
+  end
 
   resources :workdays, only: [:new, :create, :edit, :update]
   get '/workdays/on_leave', to: 'workdays#on_leave'
